d824c6dcbc854bad0c237289e181f6216ea416e5
mdabdulaziztanvir <abdulazizztanvir@gmail.com>
2023-12-18 20:35:31 +0600

    adding requet and response in node js
    i am leaning this from a youtube channel name net ninja. he is i a good instructor

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
 
 const http = require('http');
+const fs = require('fs'); 
 
 const server = http.createServer((req, res) =>{
-    console.log('req made');
+    console.log(req.url, req.method);
+
+    // set header content type
+    res.setHeader('content-type', 'text/html');
+    // send an html file
+
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('location', '/about');
+            res.end();
+            break;
+        default:
+           path += '404.html';
+           res.statusCode = 404;
+           break;
+        
+    };
+
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
+
 });
 
 server.listen(3000, 'localhost', () => {
