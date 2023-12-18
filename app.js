ab5478a0e4e5ca79daef216b7e0dd53ba3f8b695
mdabdulaziztanvir <abdulazizztanvir@gmail.com>
2023-12-18 23:47:05 +0600

    leaning the ejs view engine for the node js/ express framework

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 
 const app = express();
-
+// register view engine
+app.set('view engine', 'ejs');
 
 
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname } );
-
+    const blogs =[
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Yoshi finds eggs2', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Yoshi finds eggs3', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', {title: 'home', blogs} );
 });
+
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname } );
+    res.render('about', {title: 'About'});
 
 });
 // redirects
@@ -20,7 +26,11 @@ app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', {title: 'Create'});
+});
+
 // page not found 404
 app.use((req, res) =>{
-    res.status(404).sendFile('./views/404.html', {root:__dirname} );
+    res.status(404).render('404', {title: '404'});
 });
